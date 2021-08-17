86dc748caed584bf3ea725c967a27f08f6d848ff
naisiaye <naisiayee@gmail.com>
2021-08-17 17:24:19 -0500

    First commit

---
+++ src/main/java/org/launchcode/StLouisAttractions/StLouisAttractionsApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.StLouisAttractions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StLouisAttractionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StLouisAttractionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/StLouisAttractions/StLouisAttractionsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.StLouisAttractions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StLouisAttractionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
