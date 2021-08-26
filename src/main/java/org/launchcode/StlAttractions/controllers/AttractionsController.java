6884c7e6f0f78e62aee9a8562df8ce44caab00c7
naisiaye <naisiayee@gmail.com>
2021-08-25 20:19:02 -0500

    login/reg done

---
+++ src/main/java/org/launchcode/StlAttractions/controllers/AttractionsController.java
@@ -0,0 +1,13 @@
+package org.launchcode.StlAttractions.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AttractionsController {
+    @GetMapping("/attractions")
+    public String attractions() {
+        return "attractions";
+    }
+}
